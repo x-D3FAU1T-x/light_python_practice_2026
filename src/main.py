3d3d271645ce2d3694aee56c22d14355c12bed45
x-D3FAU1T-x <141998589+x-D3FAU1T-x@users.noreply.github.com>
2026-06-26 11:22:02 +0700

    Добавление дерева папок

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from pathlib import Path
-from scanner import collect_items
+from scanner import collect_items, show_tree
 from report import make_report
 
 def main():
     folder_input = input("Введите путь к папке: ").strip()
     folder_path = Path(folder_input)
+
     if not folder_path.exists():
         print("Ошибка: указанная папка не существует.")
         return
@@ -13,6 +14,10 @@ def main():
         print("Ошибка: путь должен указывать на папку, а не на файл.")
         return
 
+    print("\nДерево папок")
+    show_tree(folder_path)
+
+    print("\nОтчёт")
     files, dirs = collect_items(folder_path)
     make_report(files, dirs)
 
